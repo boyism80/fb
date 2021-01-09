654c0ebf6893ccaa2498ff27edf8db26c634ee4b
boyism80 <boyism80@gmail.com>
2021-01-10 05:55:23 +0900

    디스코드 웹훅

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -48,4 +48,16 @@ pipeline {
             }
         }
     }
+
+    post {
+        always {
+            discordSend description: "'Kingdom of the wind' build complete.\nBuild ${env.BUILD_NUMBER}", 
+                        footer: currentBuild.currentResult, 
+                        link: env.BUILD_URL, 
+                        result: currentBuild.currentResult, 
+                        title: "Kingdom of the Wind", 
+                        webhookURL: "https://discordapp.com/api/webhooks/797560289905082388/y1bsZom7NB61ssDyxz4h0ByE9TMr3UfCjY5j_raAecuMdiQg79FdvVbat5ZzaalmOFTC",
+                        thumbnail: "https://w.namu.la/s/ed2ca36f0585f9ad670c225e05de8ad0032447c0e9cbfa92126030bcffa0630243c1bd7c54cc3622713b05a18f09a08075f1be803f3083336f73a99d1dbed51e962cd08d2fa4dd34fecb4625b3bbd05efd56e03553b5a033ecd60cd933090f40"
+        }
+    }
 }
