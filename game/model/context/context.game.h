7f6c6c761083c1fe82c63c7a9a9576f93ea437ed
boyism80 <boyism80@gmail.com>
2021-11-12 02:22:49 +0900

    const typename 순서 잘못된거 수정

--- game/model/context/context.game.h
+++ game/model/context/context.game.h
@@ -85,7 +85,7 @@ public:
     bool                    exists(const fb::game::object& object) const;
 
     template <typename T, typename... Args>
-    T*                      make(typename const T::master* core, Args... args)
+    T*                      make(const typename T::master* core, Args... args)
     {
         return new T(*this, core, args...);
     }
