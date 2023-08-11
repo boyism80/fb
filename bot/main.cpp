f1319e7af8d21ca0cb65d60731712ae2f01b21c3
채승현 <boyism80@gmail.com>
2023-08-11 18:41:13 +0900

    boost io_context 멀티스레드 작업중

--- bot/main.cpp
+++ bot/main.cpp
@@ -5,18 +5,33 @@
 
 int main(int, char**)
 {
-	boost::asio::io_context io;
-	auto bots = fb::bot::bot_container(io);
+	constexpr auto io_size = 2;
+	constexpr auto count = 100;
+
+	boost::asio::io_context io1, io2;
+	boost::asio::executor_work_guard<decltype(io1.get_executor())> work1{ io1.get_executor() };
+	boost::asio::executor_work_guard<decltype(io2.get_executor())> work2{ io2.get_executor() };
+
+	auto bots1 = fb::bot::bot_container(io1);
+	auto bots2 = fb::bot::bot_container(io2);
 	auto endpoint = boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 3001);
 
-	auto count = 10;
-	for(int i = 0; i < count; i++)
+	for (int i = 0; i < count / io_size; i++)
 	{
-		auto bot = bots.create<fb::bot::gateway_bot>();
+		auto bot = bots1.create<fb::bot::gateway_bot>();
 		bot->connect(endpoint);
 	}
 
-	io.run();
+	for (int i = 0; i < count / io_size; i++)
+	{
+		auto bot = bots2.create<fb::bot::gateway_bot>();
+		bot->connect(endpoint);
+	}
+
+	boost::asio::thread_pool thread_pool(io_size);
+	post(thread_pool, [&io1] { io1.run(); });
+	post(thread_pool, [&io2] { io2.run(); });
+	thread_pool.join();
 
 	return 0;
 }
