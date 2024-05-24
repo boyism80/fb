98ff7e5bdb479d4c08d8029ff89440663935375d
채승현 <boyism80@gmail.com>
2024-05-24 19:24:33 +0900

    cpp_redis로 모듈 변경하고 테스트중

--- include/fb/core/redis.h
+++ include/fb/core/redis.h
@@ -4,19 +4,19 @@
 #include <functional>
 #include <queue>
 #include <set>
-#include <redis-cpp/stream.h>
-#include <redis-cpp/execute.h>
+#include <cpp_redis/cpp_redis>
 #include <fb/core/coroutine.h>
 #include <fb/core/abstract.h>
 #include <fb/core/logger.h>
+#include <boost/thread.hpp>
 
 namespace fb {
 
 class redis_stream_pool
 {
 private:
-    std::queue<std::shared_ptr<std::iostream>> _idle;
-    std::set<std::shared_ptr<std::iostream>> _busy;
+    std::queue<std::shared_ptr<cpp_redis::client>> _idle;
+    std::set<std::shared_ptr<cpp_redis::client>> _busy;
     std::mutex _mutex;
 
 public:
@@ -24,33 +24,34 @@ public:
     ~redis_stream_pool() = default;
 
 public:
-    std::shared_ptr<std::iostream> get()
+    std::shared_ptr<cpp_redis::client> get()
     {
         auto _ = std::lock_guard(_mutex);
 
         if (this->_idle.empty() == false)
         {
-            auto& stream = this->_idle.front();
-            this->_busy.insert(stream);
+            auto& client = this->_idle.front();
+            this->_busy.insert(client);
             this->_idle.pop();
 
-            return stream;
+            return client;
         }
         else
         {
-            auto stream = rediscpp::make_stream("localhost", "6379");
-            this->_busy.insert(stream);
+            auto client = std::make_shared< cpp_redis::client>();
+            client->connect();
+            this->_busy.insert(client);
 
-            return stream;
+            return client;
         }
     }
 
-    void release(std::shared_ptr<std::iostream>& stream)
+    void release(std::shared_ptr<cpp_redis::client>& client)
     {
         auto _ = std::lock_guard(_mutex);
 
-        this->_idle.push(stream);
-        this->_busy.erase(stream);
+        this->_idle.push(client);
+        this->_busy.erase(client);
     }
 };
 
@@ -66,89 +67,86 @@ public:
 	{ }
 	~redis() = default;
 
-
-public:
-	template <typename T>
-    fb::awaitable<T> sync(const std::string& key, const std::function<T()>& fn)
+private:
+    template <typename T>
+    void try_lock(const std::string& key, fb::awaitable<T>& awaitable, const std::function<T()>& fn, const std::string& uuid, std::shared_ptr<cpp_redis::subscriber> subscriber, fb::thread* thread)
     {
-        auto subscriber = this->_pool.get();
-        auto xxx = subscriber.use_count();
-        rediscpp::execute(*subscriber, "subscribe", key);
-
-        auto stream = this->_pool.get();
+        auto conn = std::make_shared< cpp_redis::client>();
+        conn->connect();
         auto script =
             "local success = redis.call('setnx', KEYS[1], 'lock')\n"
             "if success == 1 then\n"
             "    redis.call('expire', KEYS[1], 5)\n"
             "end\n"
             "return success";
-        auto response = rediscpp::execute(*stream, "eval", script, "1", key);
-        auto success = response.as<bool>();
-        this->_pool.release(stream);
-        if (success)
-        {
-            fb::logger::debug("리소스 점유에 성공");
-            rediscpp::execute(*subscriber, "unsubscribe", key);
-            this->_pool.release(subscriber);
-
-            auto callback = [this, &fn, key](auto& awaitable)
+        conn->eval(script, 1, { key }, {}, [&, this, conn, key, uuid, subscriber, thread](cpp_redis::reply& v) mutable
             {
-                auto result = fn();
-
-                auto stream = this->_pool.get();
-                rediscpp::execute(*stream, "del", key);
-                rediscpp::execute(*stream, "publish", key, "ok");
-                this->_pool.release(stream);
-
-                auto thread = this->_owner.current_thread();
-                if (thread != nullptr)
+                auto success = v.as_integer() == 1;
+                if (success)
                 {
-                    thread->dispatch([&awaitable, result](uint8_t) mutable
+                    auto action = [this](auto& awaitable, auto& fn, const std::string key, const std::string uuid, auto conn)
                     {
+                        auto result = fn();
+
+                        auto new_conn = std::make_shared< cpp_redis::client>();
+                        new_conn->connect();
+
+                        new_conn->del({ key });
+                        new_conn->publish(key, uuid, [&, this, conn, key, result](cpp_redis::reply& reply) mutable
+                            {
+                                //this->_pool.release(conn);
+                            });
+
+                        new_conn->commit();
+
                         awaitable.result = &result;
                         awaitable.handler.resume();
-                    });
+                    };
+
+                    if (thread != nullptr)
+                    {
+                        thread->dispatch([&action, &awaitable, &fn, key, uuid, conn](uint8_t) { action(awaitable, fn, key, uuid, conn); });
+                    }
+                    else
+                    {
+                        boost::thread{ [&action, &awaitable, &fn, key, uuid, conn]() { action(awaitable, fn, key, uuid, conn); } };
+                    }
+
+                    fb::logger::debug("리소스 점유에 성공");
                 }
                 else
                 {
-                    awaitable.result = &result;
-                    awaitable.handler.resume();
+                    fb::logger::debug("리소스 점유에 실패하여 대기");
                 }
-            };
+            });
+        conn->commit();
+    }
 
-            return fb::awaitable<T>(callback);
-        }
-        else
+
+public:
+	template <typename T>
+    fb::awaitable<T> sync(const std::string& key, const std::function<T()>& fn)
+    {
+        auto callback = [this, key, &fn](auto& awaitable)
         {
-            fb::logger::debug("리소스 점유에 실패하여 대기");
-            rediscpp::value value{ *subscriber };
-            return std::visit(rediscpp::resp::detail::overloaded
-            {
-                [this, &fn, &subscriber, key](const rediscpp::resp::deserialization::array& arr)
+            auto thread = this->_owner.current_thread();
+            auto uuid = boost::uuids::to_string(boost::uuids::random_generator()());
+            auto subscriber = std::make_shared<cpp_redis::subscriber>();
+            subscriber->connect();
+            subscriber->subscribe(key, [&, this, key, uuid, subscriber, thread](const std::string& chan, const std::string& msg)
                 {
-                    rediscpp::execute(*subscriber, "unsubscribe",  key);
-                    this->_pool.release(subscriber);
-                    return this->sync(key, fn);
-                },
-                [](rediscpp::resp::deserialization::error_message const& err)
-                {
-                    return fb::awaitable<T>([what = std::string(err.get().data())](auto& awaitable)
-                    {
-                        awaitable.error = std::make_unique<std::runtime_error>(what);
-                        awaitable.handler.resume();
-                    });
-                },
-                    // An unexpected response.
-                    [](auto const& x)
-                    {
-                        return fb::awaitable<T>([](auto& awaitable)
-                        {
-                            awaitable.error = std::make_unique<std::runtime_error>("unhandled exception");
-                            awaitable.handler.resume();
-                        });
-                    }
-            }, value.get());
-        }
+                    if (chan != key)
+                        return;
+
+                    if (msg != uuid)
+                        this->try_lock(key, awaitable, fn, uuid, subscriber, thread);
+                });
+            subscriber->commit();
+
+            this->try_lock(key, awaitable, fn, uuid, subscriber, thread);
+        };
+
+        return fb::awaitable<T>(callback);
     }
 };
 
