dbca63cb75c8ed2645594910f5dcb77b7e2e8d16
cshyeon <boyism80@gmail.com>
2024-11-01 00:46:28 +0900

    윈도우 개발 환경에 mysql 설치하기 싫어서 수정

--- infra/db/import.bat
+++ infra/db/import.bat
@@ -1,5 +1,12 @@
 @ECHO OFF
 SET FNAME=latest.sql
 
-mysql.exe --protocol=tcp --host=localhost --user=fb --password=admin --port=3306 --default-character-set=utf8 --comments  < %FNAME%
+mysql.exe --protocol=tcp --host=localhost --user=fb --password=admin --port=30100 --default-character-set=utf8 --comments  < %FNAME%
+mysql.exe --protocol=tcp --host=localhost --user=fb --password=admin --port=30101 --default-character-set=utf8 --comments  < %FNAME%
+mysql.exe --protocol=tcp --host=localhost --user=fb --password=admin --port=30102 --default-character-set=utf8 --comments  < %FNAME%
+mysql.exe --protocol=tcp --host=localhost --user=fb --password=admin --port=30103 --default-character-set=utf8 --comments  < %FNAME%
+mysql.exe --protocol=tcp --host=localhost --user=fb --password=admin --port=30104 --default-character-set=utf8 --comments  < %FNAME%
+mysql.exe --protocol=tcp --host=localhost --user=fb --password=admin --port=30105 --default-character-set=utf8 --comments  < %FNAME%
+mysql.exe --protocol=tcp --host=localhost --user=fb --password=admin --port=30106 --default-character-set=utf8 --comments  < %FNAME%
+mysql.exe --protocol=tcp --host=localhost --user=fb --password=admin --port=30107 --default-character-set=utf8 --comments  < %FNAME%
 ECHO DUMP COMPLETE : %FNAME%
