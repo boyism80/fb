54e8ddefa290f95bfd49924df9768afdb824cfef
boyism80 <boyism80@gmail.com>
2024-11-03 16:14:37 +0900

    update submodules and install npm

--- reset.sh
+++ reset.sh
@@ -1,3 +1,7 @@
 git reset --hard && git clean -fxd
 git fetch && git pull
 git submodule foreach git reset --hard && git clean -fxd
+git submodule update --init --recursive
+cd infra/pulumi
+npm install
+cd ../..
