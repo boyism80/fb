355e3bcc0eb8541024ff4771f67f6837a3cf4022
cshyeon <boyism80@gmail.com>
2024-11-10 14:21:40 +0900

    timespan 음수처리

--- include/fb/model/datetime.h
+++ include/fb/model/datetime.h
@@ -1,3 +1,5 @@
+// clang-format off
+
 #ifndef __DATETIME_H__
 #define __DATETIME_H__
 
@@ -16,29 +18,29 @@ private:
 
 public:
     timespan()
-    {}
+    { }
 
     timespan(const std::string& f)
     {
-        static const auto regex = boost::xpressive::sregex::compile("((?P<day>\\d+).)?(?P<hour>\\d{1,2}):(?P<min>\\d{1,2}):(?P<sec>\\d{1,2})(?:.(?P<ms>\\d+))?");
+        static const auto regex = boost::xpressive::sregex::compile("(?P<ne>-)?((?P<day>\\d+).)?(?P<hour>\\d{1,2}):(?P<min>\\d{1,2}):(?P<sec>\\d{1,2})(?:.(?P<ms>\\d+))?");
         auto what = boost::xpressive::smatch();
         if (boost::xpressive::regex_match(f, what, regex) == false)
             throw std::runtime_error(std::format("cannot parse timespan. value : {}", f));
 
-        auto day = what["day"].matched ? std::stoi(what["day"].str()) : 0;
+        auto day   = what["day"].matched ? std::stoi(what["day"].str()) : 0;
         auto hours = std::stoi(what["hour"].str());
-        auto mins = std::stoi(what["min"].str());
-        auto secs = std::stoi(what["sec"].str());
-        auto ms = 0;
+        auto mins  = std::stoi(what["min"].str());
+        auto secs  = std::stoi(what["sec"].str());
+        auto ms    = 0;
         if (what["ms"].matched)
         {
-            auto in = what["ms"].str();
+            auto in   = what["ms"].str();
             auto step = 100;
             for (int i = 0; i < std::min<uint32_t>(3, in.size()); i++)
             {
-                auto v = in.at(i) - '0';
-                ms += v * step;
-                step /= 10;
+                auto v  = in.at(i) - '0';
+                ms     += v * step;
+                step   /= 10;
             }
         }
 
@@ -47,6 +49,11 @@ public:
             + std::chrono::minutes(mins)
             + std::chrono::hours(hours)
             + std::chrono::hours(day * 24);
+
+        if (what["ne"].matched)
+        {
+            this->_ms *= -1;
+        }
     }
 
     timespan(int days, int hours, int minutes, int seconds, int milliseconds) : _ms(std::chrono::milliseconds(milliseconds)
@@ -54,135 +61,134 @@ public:
         + std::chrono::minutes(minutes)
         + std::chrono::hours(hours)
         + std::chrono::hours(days * 24))
-    {
-    }
+    { }
 
     template <class _Rep, class _Period>
     timespan(const std::chrono::duration<_Rep, _Period>& duration) : _ms(std::chrono::duration_cast<std::chrono::milliseconds>(duration))
-    {}
+    { }
 
     timespan(const timespan& ts) : _ms(ts._ms)
-    {}
+    { }
 
     ~timespan()
-    {}
+    { }
 
 public:
     template <class _Rep, class _Period>
-    operator std::chrono::duration<_Rep, _Period>() const
+    operator std::chrono::duration<_Rep, _Period> () const
     {
         return std::chrono::duration_cast<std::chrono::duration<_Rep, _Period>>(this->_ms);
     }
 
-    friend timespan operator + (const timespan& ts1, const timespan& ts2)
+    friend timespan operator+ (const timespan& ts1, const timespan& ts2)
     {
         return timespan(ts1._ms + ts2._ms);
     }
 
-    friend timespan operator - (const timespan& ts1, const timespan& ts2)
+    friend timespan operator- (const timespan& ts1, const timespan& ts2)
     {
         return timespan(ts1._ms - ts2._ms);
     }
 
     template <class _Rep, class _Period>
-    friend timespan operator + (const timespan& ts, std::chrono::duration<_Rep, _Period> duration)
+    friend timespan operator+ (const timespan& ts, std::chrono::duration<_Rep, _Period> duration)
     {
         return timespan(ts._ms + duration);
     }
 
     template <class _Rep, class _Period>
-    friend timespan operator - (const timespan& ts, std::chrono::duration<_Rep, _Period> duration)
+    friend timespan operator- (const timespan& ts, std::chrono::duration<_Rep, _Period> duration)
     {
         return timespan(ts._ms - duration);
     }
 
-    friend bool operator == (const timespan& ts1, const timespan& ts2)
+    friend bool operator== (const timespan& ts1, const timespan& ts2)
     {
         return ts1._ms == ts2._ms;
     }
 
     template <class _Rep, class _Period>
-    friend bool operator == (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
+    friend bool operator== (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
     {
         return ts1._ms == duration;
     }
 
-    friend bool operator != (const timespan& ts1, const timespan& ts2)
+    friend bool operator!= (const timespan& ts1, const timespan& ts2)
     {
         return ts1._ms != ts2._ms;
     }
 
     template <class _Rep, class _Period>
-    friend bool operator != (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
+    friend bool operator!= (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
     {
         return ts1._ms != duration;
     }
 
-    friend bool operator > (const timespan& ts1, const timespan& ts2)
+    friend bool operator> (const timespan& ts1, const timespan& ts2)
     {
         return ts1._ms > ts2._ms;
     }
 
     template <class _Rep, class _Period>
-    friend bool operator > (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
+    friend bool operator> (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
     {
         return ts1._ms > duration;
     }
 
-    friend bool operator < (const timespan& ts1, const timespan& ts2)
+    friend bool operator< (const timespan& ts1, const timespan& ts2)
     {
         return ts1._ms < ts2._ms;
     }
 
     template <class _Rep, class _Period>
-    friend bool operator < (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
+    friend bool operator< (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
     {
         return ts1._ms < duration;
     }
 
-    friend bool operator >= (const timespan& ts1, const timespan& ts2)
+    friend bool operator>= (const timespan& ts1, const timespan& ts2)
     {
         return ts1._ms >= ts2._ms;
     }
 
     template <class _Rep, class _Period>
-    friend bool operator >= (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
+    friend bool operator>= (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
     {
         return ts1._ms >= duration;
     }
 
-    friend bool operator <= (const timespan& ts1, const timespan& ts2)
+    friend bool operator<= (const timespan& ts1, const timespan& ts2)
     {
         return ts1._ms <= ts2._ms;
     }
 
     template <class _Rep, class _Period>
-    friend bool operator <= (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
+    friend bool operator<= (const timespan& ts1, std::chrono::duration<_Rep, _Period> duration)
     {
         return ts1._ms <= duration;
     }
 
-    timespan& operator += (const timespan& ts)
+    timespan& operator+= (const timespan& ts)
     {
         this->_ms += ts._ms;
         return *this;
     }
 
-    timespan& operator -= (const timespan& ts)
+    timespan& operator-= (const timespan& ts)
     {
         this->_ms -= ts._ms;
         return *this;
     }
 
     template <class _Rep, class _Period>
-    timespan& operator += (std::chrono::duration<_Rep, _Period> duration)
+    timespan& operator+= (std::chrono::duration<_Rep, _Period> duration)
     {
         this->_ms += duration;
         return *this;
     }
 
     template <class _Rep, class _Period>
-    timespan& operator -= (std::chrono::duration<_Rep, _Period> duration)
+    timespan& operator-= (std::chrono::duration<_Rep, _Period> duration)
     {
         this->_ms -= duration;
         return *this;
@@ -226,16 +232,19 @@ public:
     std::string to_string()
     {
         auto sstream = std::stringstream();
-        auto days = this->days();
-        if (days > 0)
+        if (this->_ms.count() < 0)
+            sstream << '-';
+
+        auto days = std::abs(this->days());
+        if (days != 0)
             sstream << days << '.';
 
-        sstream << std::setw(2) << std::setfill('0') << this->hours() << ':'
-            << std::setw(2) << std::setfill('0') << this->minutes() << ':'
-            << std::setw(2) << std::setfill('0') << this->seconds();
+        sstream << std::setw(2) << std::setfill('0') << std::abs(this->hours()) << ':' 
+                << std::setw(2) << std::setfill('0') << std::abs(this->minutes()) << ':' 
+                << std::setw(2) << std::setfill('0') << std::abs(this->seconds());
 
-        auto ms = this->milliseconds();
-        if (ms > 0)
+        auto ms = std::abs(this->milliseconds());
+        if (ms != 0)
             sstream << '.' << ms;
 
         return sstream.str();
@@ -329,89 +338,89 @@ public:
         return *this;
     }
 
-    friend datetime operator + (const datetime& dt, const timespan& ts)
+    friend datetime operator+ (const datetime& dt, const timespan& ts)
     {
         auto result = datetime(dt);
         result.add_timespan(ts);
         return result;
     }
 
-    friend datetime operator - (const datetime& dt, const timespan& ts)
+    friend datetime operator- (const datetime& dt, const timespan& ts)
     {
         auto result = datetime(dt);
         result.add_milliseconds(-ts.total_milliseconds());
         return result;
     }
 
-    friend timespan operator - (const datetime& dt1, const datetime& dt2)
+    friend timespan operator- (const datetime& dt1, const datetime& dt2)
     {
         auto diff = dt1._ptime - dt2._ptime;
         return timespan(boost::posix_time::to_simple_string(diff));
     }
 
-    datetime& operator += (const timespan& ts)
+    datetime& operator+= (const timespan& ts)
     {
         this->add_timespan(ts);
         return *this;
     }
 
-    datetime& operator -= (const timespan& ts)
+    datetime& operator-= (const timespan& ts)
     {
         this->add_milliseconds(-ts.total_milliseconds());
         return *this;
     }
 
-    friend bool operator == (const datetime& dt1, const datetime& dt2)
+    friend bool operator== (const datetime& dt1, const datetime& dt2)
     {
         return dt1._ptime == dt2._ptime;
     }
 
-    friend bool operator != (const datetime& dt1, const datetime& dt2)
+    friend bool operator!= (const datetime& dt1, const datetime& dt2)
     {
         return dt1._ptime != dt2._ptime;
     }
 
-    friend bool operator > (const datetime& dt1, const datetime& dt2)
+    friend bool operator> (const datetime& dt1, const datetime& dt2)
     {
         return dt1._ptime > dt2._ptime;
     }
 
-    friend bool operator < (const datetime& dt1, const datetime& dt2)
+    friend bool operator< (const datetime& dt1, const datetime& dt2)
     {
         return dt1._ptime < dt2._ptime;
     }
 
-    friend bool operator >= (const datetime& dt1, const datetime& dt2)
+    friend bool operator>= (const datetime& dt1, const datetime& dt2)
     {
         return dt1._ptime >= dt2._ptime;
     }
 
-    friend bool operator <= (const datetime& dt1, const datetime& dt2)
+    friend bool operator<= (const datetime& dt1, const datetime& dt2)
     {
         return dt1._ptime <= dt2._ptime;
     }
 
     template <class _Rep, class _Period>
-    friend datetime operator + (const datetime& dt, std::chrono::duration<_Rep, _Period> duration)
+    friend datetime operator+ (const datetime& dt, std::chrono::duration<_Rep, _Period> duration)
     {
         return datetime(dt) + timespan(duration);
     }
 
     template <class _Rep, class _Period>
-    friend datetime operator - (const datetime& dt, std::chrono::duration<_Rep, _Period> duration)
+    friend datetime operator- (const datetime& dt, std::chrono::duration<_Rep, _Period> duration)
     {
         return datetime(dt) - timespan(duration);
     }
 
     template <class _Rep, class _Period>
-    datetime& operator += (std::chrono::duration<_Rep, _Period> duration)
+    datetime& operator+= (std::chrono::duration<_Rep, _Period> duration)
     {
         (*this) += timespan(duration);
         return *this;
     }
 
     template <class _Rep, class _Period>
-    datetime& operator -= (std::chrono::duration<_Rep, _Period> duration)
+    datetime& operator-= (std::chrono::duration<_Rep, _Period> duration)
     {
         (*this) -= timespan(duration);
         return *this;
