6f4afe09e9a87f2dd0b08cfe2cc6253044c26dd3
cshyeon <boyism80@gmail.com>
2024-12-24 09:41:39 +0900

    admin 권한이 없는 명령어

--- game/lib/context/context.handler.cpp
+++ game/lib/context/context.handler.cpp
@@ -597,7 +597,7 @@ async::task<bool> context::handle_chat(fb::socket<character>& socket, const fb_r
     if (ch->inited() == false)
         co_return true;
 
-    if (ch->admin() && co_await handle_command(*ch, request.message))
+    if (co_await handle_command(*ch, request.message))
         co_return true;
 
     ch->chat(request.message, request.shout ? CHAT_TYPE::SHOUT : CHAT_TYPE::NORMAL);
