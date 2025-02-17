a5a9e382e82c7ef466e890ac5bc871610819c0fe
boyism80 <boyism80@gmail.com>
2025-02-17 19:02:21 +0900

    #59 체력사기

--- game/scripts/npc/영혼사.lua
+++ game/scripts/npc/영혼사.lua
@@ -1,8 +1,83 @@
 function on_interact(me, npc)
 
-::ROUTINE_1::
+::ROOT::
     local index, button = npc:list(me, '그래도 버티고 서서 도대체 원하는 것이 뭐냐?', {'힘올리기', '지력올리기', '민첩올리기', '체력사기', '마력사기', '성형', '성전환'}, false)
-    if index == 5 then
+    if index == 3 then
+::ROUTINE_BUY_HP_1::
+        button = npc:dialog(me, '체력을 사기 전에 명심하게.. 지금 착용하고 있는 모든 아이템을 벗은 후, 체력을 사기 바라네.', false, true)
+        if button == DIALOG_RESULT_QUIT then
+            return
+        end
+
+::ROUTINE_BUY_HP_2::
+        button = npc:dialog(me, '만약, 내 말을 듣지 않고, 체력을 사서 일어나는 불이익에 대해서는 절대 책임을 질 수 없네!!', true, true)
+        if button == DIALOG_RESULT_QUIT then
+            return
+        end
+
+        if button == DIALOG_RESULT_PREV then
+            goto ROUTINE_BUY_HP_1
+        end
+
+::ROUTINE_BUY_HP_3::
+        button = npc:dialog(me, '경험치로 체력을 사고 싶다고.... 늘 이렇게 경험치를 팔고는 여기 저기 가서 잘못 팔았다고 후회를 할 것이거늘... 쯧쯧... 어디 레벨이나 되는지 한 번 보자.', true, true)
+        if button == DIALOG_RESULT_QUIT then
+            return
+        end
+
+        if button == DIALOG_RESULT_PREV then
+            goto ROUTINE_BUY_HP_2
+        end
+
+        button = npc:dialog(me, '레벨은 충분하고...', true, true)
+        if button == DIALOG_RESULT_QUIT then
+            return
+        end
+
+        if button == DIALOG_RESULT_PREV then
+            goto ROUTINE_BUY_HP_3
+        end
+
+        local step = npc:input(me, '그래 체력을 몇 단계나 올리려고? 체력은 한 단계가 50이라는 것은 알고 있겠지?', '예,', '단계를 올리고 싶습니다.')
+        local exp = step * 10000000
+        local my_exp = me:exp()
+        if my_exp < exp then
+            npc:dialog(me,  '경험치가 모자람')
+            goto ROOT
+        end
+
+        me:exp(my_exp - exp)
+        me:base_hp(me:base_hp() + (50 * step))
+
+::ROUTINE_BUY_HP_5::
+        button = npc:dialog(me, string.format('자네의 경험치로 체력을 %d단계 올렸으니 그리 알고 돌아가도록 해.', step), false, true)
+        if button == DIALOG_RESULT_QUIT then
+            return
+        end
+
+::ROUTINE_BUY_HP_6::
+        button = npc:dialog(me, string.format('화면에 바로 보일런지는 모르겠지만, 자네의 체력은 이제 %d이네', me:maxhp()), true, true)
+        if button == DIALOG_RESULT_QUIT then
+            return
+        end
+
+        if button == DIALOG_RESULT_PREV then
+            goto ROUTINE_BUY_HP_5
+        end
+
+::ROUTINE_BUY_HP_7::
+        button = npc:dialog(me, '강인한 체력을 나쁜 곳에 쓰려고 한다면... 내가 자네를 가만두지 않겠네. 알겠나?', true, true)
+        if button == DIALOG_RESULT_QUIT then
+            return
+        end
+
+        if button == DIALOG_RESULT_PREV then
+            goto ROUTINE_BUY_HP_6
+        else
+            goto ROOT
+        end
+
+    elseif index == 5 then
         index, button = npc:list(me, '지금의 모습을 벗어나 새로운 얼굴을 갖기를 원하고 있나?', {'예', '아니오'})
         if index ~= 0 then
             return
@@ -18,7 +93,7 @@ function on_interact(me, npc)
             return
         end
 
-::ROUTINE_2::
+::ROUTINE_CHANGE_FACE_1::
         button = npc:dialog(me, '지금부터 차례로 자네가 갖을 수 있는 모습을 보여주도록 하겠다. 수술이 그리 쉽지는 않고', false, true)
         if button == DIALOG_RESULT_QUIT then
             return
@@ -26,7 +101,7 @@ function on_interact(me, npc)
 
         button = npc:dialog(me, '원하는 모습이 보이면 바로 예를 선택하도록. 현재 네가 가질 수 있는 얼굴은 101개가 있다. 이전이나 다음을 선택해도 얼굴이 바뀌지 않으면 마지막 모습이니 그리 알도록.', true, true)
         if button == DIALOG_RESULT_PREV then
-            goto ROUTINE_2
+            goto ROUTINE_CHANGE_FACE_1
         end
 
         if button == DIALOG_RESULT_QUIT then
@@ -50,7 +125,7 @@ function on_interact(me, npc)
                     if npc:dialog(me, '백만원이 없잖아 임마', false, true) == DIALOG_RESULT_QUIT then
                         return
                     end
-                    goto ROUTINE_1
+                    goto ROOT
                 end
 
                 me:look(face)
@@ -63,7 +138,7 @@ function on_interact(me, npc)
                     return
                 end
 
-                goto ROUTINE_1
+                goto ROOT
             elseif index == 1 then
                 face = (face - 1 + 102) % 102
             elseif index == 2 then
