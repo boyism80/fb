c7b5681c959d5258fa07fbbbb430fb0f12f63375
boyism80 <boyism80@gmail.com>
2025-03-15 18:39:56 +0900

    스킬 사운드 누락된거 추가

--- game/scripts/spell/비영승보.lua
+++ game/scripts/spell/비영승보.lua
@@ -46,6 +46,7 @@ function ON_CAST_2015(me, spell)
             me:attack(14)
             me:action(ACTION_CAST_SPELL, 25)
             me:message('비영승보를 외웠습니다.')
+            me:sound(30)
             break
         end
     end
