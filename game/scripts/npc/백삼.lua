e2a8a9f860ad8696110607f12eb386db498db89d
boyism80 <boyism80@gmail.com>
2025-03-17 17:43:00 +0900

    아이템 판매 NPC

--- game/scripts/npc/백삼.lua
+++ game/scripts/npc/백삼.lua
@@ -1,3 +1,19 @@
 function NPC_208(me, npc)
+::NPC_208_000::
+    local selected = npc:list(me, '안녕하세요. 무엇을 도와드릴까요?', {'물건 사기', '물건 팔기'})
+    if selected == nil then
+        return
+    end
 
+    if selected == 0 then
+        if NPC_SELL_DIALOG(me, npc) == DIALOG_RESULT_NEXT then
+            goto NPC_208_000
+        end
+    elseif selected == 1 then
+        if NPC_BUY_DIALOG(me, npc) == DIALOG_RESULT_NEXT then
+            goto NPC_208_000
+        end
+    else
+
+    end
 end
---
+++ game/scripts/npc/왈숙네.lua
@@ -0,0 +1,35 @@
+function NPC_7(me, npc)
+::NPC_7_000::
+    local selected = npc:list(me, '안녕하세요. 무엇을 도와드릴까요?', {'물건 사기', '물건 팔기', '금전 맡기기', '물건 맡기기', '금전 찾기', '물건 찾기'})
+    if selected == nil then
+        return
+    end
+
+    if selected == 0 then
+        if NPC_SELL_DIALOG(me, npc) == DIALOG_RESULT_NEXT then
+            goto NPC_7_000
+        end
+    elseif selected == 1 then
+        if NPC_BUY_DIALOG(me, npc) == DIALOG_RESULT_NEXT then
+            goto NPC_7_000
+        end
+    elseif selected == 2 then
+    	if NPC_HOLD_MONEY_DIALOG(me, npc) == DIALOG_RESULT_NEXT then
+           goto NPC_7_000 
+        end
+    elseif selected == 3 then
+    	if NPC_HOLD_ITEM_DIALOG(me, npc) == DIALOG_RESULT_NEXT then
+           goto NPC_7_000 
+        end
+    elseif selected == 4 then
+        if NPC_RETURN_MONEY_DIALOG(me, npc) == DIALOG_RESULT_NEXT then
+           goto NPC_7_000 
+        end
+    elseif selected == 5 then
+        if NPC_RETURN_ITEM_DIALOG(me, npc) == DIALOG_RESULT_NEXT then
+           goto NPC_7_000 
+        end
+    else
+
+    end
+end
