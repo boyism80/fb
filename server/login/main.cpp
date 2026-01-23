b6733c1dad1149566c5bb4c040982274c057e365
boyism80 <boyism80@gmail.com>
2026-01-23 11:30:02 +0900

    feat: add encoding function support to model option
    - Add encoding_func member variable and encoding()/encode() functions to model::option
    - Replace UTF8 direct calls with encoding function in const_map templates
    - Add encoding function setup in game and login server main.cpp

--- server/login/main.cpp
+++ server/login/main.cpp
@@ -55,6 +55,7 @@ int main(int argc, char** argv)
         ::set_console_icon(IDI_BARAM);
         ::SetConsoleTitle(CONSOLE_TITLE);
         fb::model::option::decoding(cp949);
+        fb::model::option::encoding(utf8);
         flatbuffers::option::encoding(utf8);
         flatbuffers::option::decoding(cp949);
 #endif
